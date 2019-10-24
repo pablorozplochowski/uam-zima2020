33a966a4996b296b03202b33de5c10d7139a0dae
software-wizard <przemek@software-wizard.pl>
2019-10-24 10:39:27 +0200

    X

--- battle-engine/src/test/java/pl.psi.battleengine/CreatureTest.java
+++ battle-engine/src/test/java/pl.psi.battleengine/CreatureTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CreatureTest {
 
     @Test
-    void attackedCreatureShouldLostTwoHp(){
+    void attackedImpShouldLostThreeHp(){
         //given
         Creature centaur = new Creature("centaur",10,5,3);
         Creature imp = new Creature("imp", 5,2,2);
@@ -16,5 +16,15 @@ class CreatureTest {
         centaur.attack(imp);
         //then
         assertEquals(2, imp.getCurrentHp());
+    }
+
+    @Test
+    void attackedCentaurShouldLost(){
+        Creature centaur = new Creature("centaur",10,5,3);
+        Creature imp = new Creature("imp", 5,2,2);
+
+        imp.attack(centaur);
+
+        assertEquals(9, centaur.getCurrentHp());
     }
 }
