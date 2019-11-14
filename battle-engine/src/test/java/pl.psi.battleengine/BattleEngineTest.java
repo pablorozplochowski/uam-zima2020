086a5490eafe202aa3e16f35e8926e464916519d
software-wizard <przemek@software-wizard.pl>
2019-11-14 10:45:38 +0100

    extends first test

--- battle-engine/src/test/java/pl.psi.battleengine/BattleEngineTest.java
+++ battle-engine/src/test/java/pl.psi.battleengine/BattleEngineTest.java
@@ -11,12 +11,24 @@ class BattleEngineTest {
     @Test
     void getByPointZeroOneShouldReturnCreature(){
         Hero heroWithCreature = new Hero();
-        heroWithCreature.addCreature(Creature.builder().aName("Creature").build());
+        heroWithCreature.addCreature(Creature.builder().aName("C1").build());
+        heroWithCreature.addCreature(Creature.builder().aName("C2").build());
+        heroWithCreature.addCreature(Creature.builder().aName("C3").build());
+        heroWithCreature.addCreature(Creature.builder().aName("C4").build());
+        heroWithCreature.addCreature(Creature.builder().aName("C5").build());
         BattleEngine engine = new BattleEngine(heroWithCreature, new Hero());
 
-        GuiTileIf result = engine.getByPoint(new Point(0, 1));
+        GuiTileIf result1 = engine.getByPoint(new Point(0, 1));
+        GuiTileIf result2 = engine.getByPoint(new Point(0, 3));
+        GuiTileIf result3 = engine.getByPoint(new Point(0, 5));
+        GuiTileIf result4 = engine.getByPoint(new Point(0, 7));
+        GuiTileIf result5 = engine.getByPoint(new Point(0, 9));
 
-        assertEquals("Creature", result.getIcon());
+        assertEquals("C1", result1.getIcon());
+        assertEquals("C2", result2.getIcon());
+        assertEquals("C3", result3.getIcon());
+        assertEquals("C4", result4.getIcon());
+        assertEquals("C5", result5.getIcon());
     }
 
 }
